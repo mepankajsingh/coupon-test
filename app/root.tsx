4393bd5f21b4a154cbd06250d1b7623c5f6d9957
mepankajsingh <154048622+mepankajsingh@users.noreply.github.com>
2025-04-15 19:42:04 +0530

    Initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { SpeedInsights } from "@vercel/speed-insights/remix";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
 import LoadingIndicator from "~/components/LoadingIndicator";
@@ -42,6 +43,7 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <SpeedInsights />
       </body>
     </html>
   );
